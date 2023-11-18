a32c616a4dd639b65cdd7c1e3ccbca35f0c0c728
Iamneo <techadmin@iamneo.ai>
2023-11-18 07:57:16 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'angularapp';
+  employeeName:string='John Doe';
+
+  isPresent:boolean=true;
+  onToggleAttendance(){
+    this.isPresent=!this.isPresent;
+    
+  }
+  imageUrl='https://test-image-url.com/johndoe.jpg';
+  colSpan=2;
+
+  isSpecial=true;
+
+  textColor='red';
+  employeeStatus='Test Employee Status';
+}
